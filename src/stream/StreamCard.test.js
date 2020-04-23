c10565f63618302c6499ffaa334ce4f9d745cc82
Samuel Durand <Samuel.Durand@digital.homeoffice.gov.uk>
2020-04-23 06:21:40 +0100

    Replace direct link by modal

--- src/stream/StreamCard.test.js
+++ src/stream/StreamCard.test.js
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom";
 import React from "react";
 import {StreamCard} from "./StreamCard";
 import {shallow} from "enzyme";
-import SearchStreams from "./SearchStreams";
 
 describe("StreamCard", () => {
     it("renders without crashing", () => {
